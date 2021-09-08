5cecf88ef897462c28d3204e44d3f2b9ec265c24
Ashraf Hafiz <ashraf.hafiz@yahoo.com>
2021-09-08 13:38:19 +0200

    first commit

---
+++ core/Router.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace app\core;
+
+
+class Router
+{
+    public Request $request;
+    protected $routes = [];
+
+    /**
+     * Router constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+        if ($callback === false) {
+            echo 'Not Found';
+            exit;
+        }
+        echo call_user_func($callback);
+        var_dump($callback);
+    }
+
+}
